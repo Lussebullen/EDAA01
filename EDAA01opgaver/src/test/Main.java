aa8c4e22855ea6720709454382a11d2521c8820e
Asger <Asger@DESKTOP-9CUQD0P>
2021-01-22 13:49:26 +0100

    Test doc for running sample code

---
+++ EDAA01opgaver/src/test/Main.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Set<Integer> nbrs = new LinkedHashSet<Integer>();
+		for (int i = 0; i < 100; i += 10) {
+			nbrs.add(i);
+			nbrs.add(i); // notera: talet läggs till två gånger
+		}
+		for (int a : nbrs) {
+		System.out.println(a);
+		}
+	}
+
+}

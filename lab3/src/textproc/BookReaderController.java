5be92892f0cc64dac1f0a93426131d6d9be4b1b4
Mathias Duedahl <mathias.duedahl@gmail.com>
2021-02-09 17:17:16 +0100

    V4, V1, V2

--- lab3/src/textproc/BookReaderController.java
+++ lab3/src/textproc/BookReaderController.java
@@ -5,6 +5,7 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -12,13 +13,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.swing.ButtonGroup;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 
@@ -41,13 +48,26 @@ public class BookReaderController {
 		//
 		
 		//D5
-		JButton fbut = new JButton("Frequency");
-		JButton abut = new JButton("Alphabetic");
+		//JButton fbut = new JButton("Frequency");
+		//JButton abut = new JButton("Alphabetic");
 		
+		//JPanel panel = new JPanel();
+		//panel.add(fbut);
+		//panel.add(abut);
+		
+		//pane.add(panel, BorderLayout.SOUTH);
+		//
+		
+		//V4
+		ButtonGroup group = new ButtonGroup();
+
+		JRadioButton fbut = new JRadioButton("Frequency");
+		JRadioButton abut = new JRadioButton("Alphabetic");
+		group.add(fbut);
+		group.add(abut);
 		JPanel panel = new JPanel();
 		panel.add(fbut);
 		panel.add(abut);
-		
 		pane.add(panel, BorderLayout.SOUTH);
 		//
 		
@@ -67,13 +87,19 @@ public class BookReaderController {
 		});
 		//
 		
-		//D8
-		JButton nbut = new JButton("Search:");
+		//D8 & V2
+		JButton nbut = new JButton("Find:");
 		JTextField field = new JTextField("",15);  //empty textfield with width of 15 cols.
 		panel.add(nbut);
 		panel.add(field);
 		nbut.addActionListener(event -> {
 			String str = field.getText();
+			
+			//V1
+			str=str.toLowerCase();
+			str=str.strip();
+			//
+			
 			int indx = -1;
 			for (int i = 0; i <listModel.getSize(); i++) {
 				if (listModel.getElementAt(i).getKey().equals(str)) {
@@ -83,9 +109,13 @@ public class BookReaderController {
 			if (indx>=0) {
 				listView.ensureIndexIsVisible(indx);
 				listView.setSelectedIndex(indx);
+			} else { //V2
+				JOptionPane popup = new JOptionPane();
+				JOptionPane.showMessageDialog(popup,"The word "+str+" does not occur in the book."); 
 			}
 		});
 		//
+        
 		
 		// pane är en behållarkomponent till vilken de övriga komponenterna(listvy, knappar etc.) ska läggas till.
 		frame.pack();

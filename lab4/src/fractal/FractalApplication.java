f7b0d9e93e561e25887a251889ad9ba68c144c87
Mathias Duedahl <mathias.duedahl@gmail.com>
2021-02-16 16:15:11 +0100

    D4, still error in middle triangle overlap. To be corrected in D5.

--- lab4/src/fractal/FractalApplication.java
+++ lab4/src/fractal/FractalApplication.java
@@ -12,7 +12,7 @@ public class FractalApplication {
 		Point a = new Point(50,50);
 		Point b = new Point(100,500);
 		Point c = new Point(400,300);
-		fractals[1] = new Mountain(300, a, b, c);
+		fractals[1] = new Mountain(300, a, b, c, 20);
 	    new FractalView(fractals, "Fraktaler", 600, 600);
 	}
 
--- lab4/src/mountain/Mountain.java
+++ lab4/src/mountain/Mountain.java
@@ -8,13 +8,15 @@ public class Mountain extends Fractal {
 	private Point a;
 	private Point b;
 	private Point c;
+	private double dev;
 	
-	public Mountain(int length, Point a, Point b, Point c) {
+	public Mountain(int length, Point a, Point b, Point c, double dev) {
 		super();
 		this.length = length;
 		this.a = a;
 		this.b = b;
 		this.c = c;
+		this.dev = dev;
 	}
 	
 	@Override
@@ -26,22 +28,53 @@ public class Mountain extends Fractal {
 	@Override
 	public void draw(TurtleGraphics turtle) {
 		// TODO Auto-generated method stub	
-		drawTriangle(turtle, a, b, c, order);
-			
+		drawTriangle(turtle, a, b, c, order, dev);
 		}
-		
-	private void drawTriangle(TurtleGraphics turtle, Point a, Point b, Point c, int order) {
-		drawLine(turtle, a,b);
-		drawLine(turtle, b,c);
-		drawLine(turtle, a, c);
-		if (order!=0) {
-			Point pab = new Point((int)((a.getX()+b.getX())/2),(int)((a.getY()+b.getY())/2));
-			Point pbc = new Point((int)((c.getX()+b.getX())/2),(int)((c.getY()+b.getY())/2));
-			Point pac = new Point((int)((a.getX()+c.getX())/2),(int)((a.getY()+c.getY())/2));
-			drawTriangle(turtle, pab, pbc, pac, order-1);
-			drawTriangle(turtle, a, pab, pac, order-1);
-			drawTriangle(turtle, b, pbc, pab, order-1);
-			drawTriangle(turtle, c, pbc, pac, order-1);
+
+// D3
+//	private void drawTriangle(TurtleGraphics turtle, Point a, Point b, Point c, int order) {
+//		drawLine(turtle, a,b);
+//		drawLine(turtle, b,c);
+//		drawLine(turtle, a, c);
+//		if (order!=0) {
+//			Point pab = new Point((int)((a.getX()+b.getX())/2),(int)((a.getY()+b.getY())/2));
+//			Point pbc = new Point((int)((c.getX()+b.getX())/2),(int)((c.getY()+b.getY())/2));
+//			Point pac = new Point((int)((a.getX()+c.getX())/2),(int)((a.getY()+c.getY())/2));
+//			drawTriangle(turtle, pab, pbc, pac, order-1);
+//			drawTriangle(turtle, a, pab, pac, order-1);
+//			drawTriangle(turtle, b, pbc, pab, order-1);
+//			drawTriangle(turtle, c, pbc, pac, order-1);
+//		}
+//	}
+	
+	//D4
+	private void drawTriangle(TurtleGraphics turtle, Point a, Point b, Point c, int order, double dev) {
+		if (order==0) {
+			drawLine(turtle, a,b);
+			drawLine(turtle, b,c);
+			drawLine(turtle, a, c);
+		} else if (order==1) {
+			Point pab = new Point((int)((a.getX()+b.getX())/2),(int)((a.getY()+b.getY())/2+RandomUtilities.randFunc(dev)));
+			Point pbc = new Point((int)((c.getX()+b.getX())/2),(int)((c.getY()+b.getY())/2+RandomUtilities.randFunc(dev)));
+			Point pac = new Point((int)((a.getX()+c.getX())/2),(int)((a.getY()+c.getY())/2+RandomUtilities.randFunc(dev)));
+			drawLine(turtle,a,pab);
+			drawLine(turtle,pab,b);
+			drawLine(turtle,b,pbc);
+			drawLine(turtle,pbc,c);
+			drawLine(turtle,c,pac);
+			drawLine(turtle,pac,a);
+			drawLine(turtle,pab,pbc);
+			drawLine(turtle,pab,pac);
+			drawLine(turtle,pbc,pac);
+		} else {
+			Point pab = new Point((int)((a.getX()+b.getX())/2),(int)((a.getY()+b.getY())/2+RandomUtilities.randFunc(dev)));
+			Point pbc = new Point((int)((c.getX()+b.getX())/2),(int)((c.getY()+b.getY())/2+RandomUtilities.randFunc(dev)));
+			Point pac = new Point((int)((a.getX()+c.getX())/2),(int)((a.getY()+c.getY())/2+RandomUtilities.randFunc(dev)));
+			
+			drawTriangle(turtle, pab, pbc, pac, order-1, dev/2);
+			drawTriangle(turtle, a, pab, pac, order-1, dev/2);
+			drawTriangle(turtle, b, pbc, pab, order-1, dev/2);
+			drawTriangle(turtle, c, pbc, pac, order-1, dev/2);
 		}
 	}
 		
